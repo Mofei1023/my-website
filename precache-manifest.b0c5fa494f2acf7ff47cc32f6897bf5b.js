529be998b7b6f6f7d5677f69e317e6dc71561e75
Mofei1023 <71375565+Mofei1023@users.noreply.github.com>
2020-10-14 04:37:24 +0800

    Updates

---
+++ precache-manifest.b0c5fa494f2acf7ff47cc32f6897bf5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7319b84b736915d51155e277f4b8fbe4",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "520d0fe5548dbdb09abb",
+    "url": "/my-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e50cb25ab13cb513b3a",
+    "url": "/my-website/static/js/2.16e07a5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.16e07a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520d0fe5548dbdb09abb",
+    "url": "/my-website/static/js/main.8b292d85.chunk.js"
+  },
+  {
+    "revision": "6a85c44cdbf6666048d3",
+    "url": "/my-website/static/js/runtime-main.9e97d7b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
